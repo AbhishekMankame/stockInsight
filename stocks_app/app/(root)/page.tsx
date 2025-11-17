4c8ff4a7feb5f4b2f1270334e3fde01ec7c9df16
Abhishek Mankame <61981517+AbhishekMankame@users.noreply.github.com>
2025-11-17 04:56:08 +0000

    layout update

---
+++ stocks_app/app/(root)/page.tsx
@@ -0,0 +1,12 @@
+import {Button} from "@/components/ui/button"
+
+const Home = () => {
+  return (
+    <div className = "flex justify-center item-center h-screen">
+
+      <button>Click me</button>
+    </div>
+  )
+}
+
+export default Home
